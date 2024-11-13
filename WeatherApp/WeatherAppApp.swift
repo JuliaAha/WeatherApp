1d51a4e56ea818cfba260a142097abdb95c802c6
JuliaAha <julg@kth.se>
2024-11-13 10:57:47 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Julia Lofland Gustafsson on 2024-11-13.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
